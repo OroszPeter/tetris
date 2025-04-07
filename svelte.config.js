147056527281af3dbc75c17de92b1638462f9290
OroszPeter <petiorosz4@gmail.com>
2025-04-07 12:19:46 +0200

    Fix deployment configuration

--- svelte.config.js
+++ svelte.config.js
@@ -15,8 +15,9 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : '/tetris'
-		}
+			base: process.env.NODE_ENV === 'production' ? '/tetris' : ''
+		},
+		appDir: 'app'
 	},
 	preprocess: vitePreprocess()
 };
